b8b1fb6bd4f048d6d6bd8b7744f20220ddd9e462
Michal Voscek <michal.voscek@gmail.com>
2022-12-02 10:53:16 +0100

    Add react router and basic page structure

---
+++ src/pages/Detail.tsx
@@ -0,0 +1,8 @@
+
+export const Detail = () => {
+  return (
+    <div>
+      <h2>Detail</h2>
+    </div>
+  )
+}
---
+++ src/pages/Search.tsx
@@ -0,0 +1,8 @@
+
+export const Search = () => {
+  return (
+    <div>
+      <h2>Search</h2>
+    </div>
+  )
+}
