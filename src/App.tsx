56b6ecdba6364e553ea1751e844d7e50bdcc923b
Michal Voscek <michal.voscek@gmail.com>
2022-12-05 13:44:38 +0100

    Add paginated list to Search page

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {Topbar} from './components/Topbar'
 export default function App() {
   return (
     <div>
-      <Topbar />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -28,30 +27,7 @@ export default function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          <li>
-            <Link to="/detail">Detail</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
+      <Topbar />
       <Outlet />
     </div>
   );
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,67 @@
 import {useGetSearchQuery} from '../features/search/moviesApi'
 import {useSearchParams} from 'react-router-dom'
 
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import ListItemButton from '@mui/material/ListItemButton'
+import Container from '@mui/material/Container'
+import Pagination from '@mui/material/Pagination'
+import Stack from '@mui/material/Stack'
+
 export const Search = () => {
-  const [searchParams] = useSearchParams()
-  const res = useGetSearchQuery({page: 1, query: searchParams.get('s')!})
+  const [searchParams, setSearchParams] = useSearchParams()
+  const searchQuery: string = searchParams.get('s')!
+  const pageQuery: string | null = searchParams.get('p')
+  const page: number = Number(pageQuery) || 1
+  const res = useGetSearchQuery({page: page, query: searchQuery})
+  const onPageChange = (e: React.ChangeEvent<unknown>, newPage: number) => {
+    searchParams.set('p', newPage.toString())
+    setSearchParams(searchParams)
+  }
+  if (res.isLoading) return <Typography>Loading...</Typography>
+  if (res.data.Response === 'False') return <Typography>{res.data.Error}</Typography>
+  console.log('res', res)
   return (
-    <div>
-      <h2>Search</h2>
-      {JSON.stringify(res)}
-    </div>
+    <Container maxWidth="sm">
+      <Typography>Search result</Typography>
+      <List sx={{width: '100%', maxWidth: 720, bgcolor: 'background.paper'}}>
+      {res.data.Search.map((movie: any, index: number) => {
+        return (<>
+          {index !== 0 && <Divider component="li" />}
+          <ListItem alignItems="flex-start" disablePadding>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src={movie.Poster} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={movie.Title}
+                secondary={
+                  <>
+                    <Typography
+                      sx={{ display: 'inline' }}
+                      component="span"
+                      variant="body2"
+                      color="text.primary"
+                    >
+                      {movie.Year}
+                    </Typography>
+                    {` - ${movie.Type}`}
+                  </>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+        </>)
+      })}
+      </List>
+      <Stack spacing={2}>
+        <Pagination onChange={onPageChange} count={Math.ceil(res.data.totalResults / 10)} />
+      </Stack>
+    </Container>
   )
 }
