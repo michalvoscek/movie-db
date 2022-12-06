0439a48059938df2f5cc22d39cac35b3b42c1471
Michal Voscek <michal.voscek@gmail.com>
2022-12-06 18:01:37 +0100

    Remove useless files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,10 @@
 import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import favouritesReducer from '../features/favourites/favouritesSlice'
-//import searchReducer from '../features/search/searchSlice'
 import {moviesApi} from '../features/search/moviesApi'
 import {loadState} from './browserStorage'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     favourites: favouritesReducer,
     [moviesApi.reducerPath]: moviesApi.reducer,
   },
